7767effdbb501f5e7fcd7bc35a015697033761be
Arman-Kabir <armankabir8@gmail.com>
2023-01-17 13:53:06 +0600

    collection of argument

--- Function-MH/app.py
+++ Function-MH/app.py
@@ -11,8 +11,18 @@
 # In programming, we have 2 types of functions...1- Perform a task... 2- Calculate and return a value.
 # In python - All functions by default return a none value.None is an object that represents absence of a value.
 
-def increment(number, by):
-    return number+by
+# def increment(number, by):
+#     return number+by
+# print(increment(2,by=1)) #keyword argument
 
-print(increment(2,by=1)) #keyword argument
-    
+def multiply(*numbers):  # Collection of arguments
+    # return x*y
+    # print(numbers)
+    total = 1
+    for number in numbers:
+        total *= number
+        # print(number)
+    return total
+
+
+print(multiply(2, 3, 4, 5))
