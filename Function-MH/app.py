03a3f69ae53197308ba25a731ef996a4f1bbfd0a
Arman-Kabir <armankabir8@gmail.com>
2023-01-17 13:18:03 +0600

    function parameter done

--- Function-MH/app.py
+++ Function-MH/app.py
@@ -1,5 +1,12 @@
-#print()
-#round()
-# def-- used for define
+# print()
+# round()
+# def-- used for define function... print()--> takes an input.
+# Paramater is input that define your function, argument is the actual value for a given parameter.
 
-def
+def greet(fn, ln):
+    print(f"Hi{fn}{ln} ")
+    print("Welcome aboard")
+
+
+greet("Mosh", "Hamedani")
+greet("John")
