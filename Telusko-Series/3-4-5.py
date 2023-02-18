9e6b8ff7c7cf0f31de00254bec824c9ba93e6b2f
Arman-Kabir <armankabir8@gmail.com>
2023-02-18 09:37:38 +0600

    python list

---
+++ Telusko-Series/3-4-5.py
@@ -0,0 +1,32 @@
+
+# 4// Variables
+# name = 'youtube'
+# print(name[0])
+# print(name[-1])
+
+# # strings in python is immutable -- string is a collection of characters
+
+# myname = 'arman kabir'
+# print(len(myname))
+
+
+# 5 // List -- similar as array
+# List is mutable- means we can change the values - add , remove values
+# We can have a list with different types of data
+#       0   1   2   3   4
+#       -5  -4  -3  -2  -1
+# nums = [25, 12, 36, 95, 14]
+# nums.append(45)
+# nums.insert(2,77)
+# nums.remove(12)
+# nums.pop(2)
+# nums.extend([29,12,14,36])
+# print(nums)
+# print(sum(nums))   # min,max,sum,sort()
+# # print(nums[1:3])
+# # print(nums[-2])
+
+# names = ['navin','kiran','john']
+# values = [9.5,'navin',25]
+# mil = [nums,names]
+# print(mil)
