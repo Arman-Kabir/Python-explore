3aadf71edba621b3a595a7071c41db1f8f45be38
Arman-Kabir <armankabir8@gmail.com>
2023-02-24 11:12:31 +0600

    dictionary done

--- Telusko-Series/6-7.py
+++ Telusko-Series/6-7.py
@@ -8,12 +8,12 @@
 tup = (21, 36, 14, 25)
 # tup[1] = 33    // 'tuple' object does not support item assignment
 # print(tup.count())
-print(tup[1])
+# print(tup[1])
 
 # Set -- set never follows a sequence. - set uses concepts of hash. Using hash we improve the performance
 # In set , indexing is not supported
-s= {22,25,14,21,5}
-print(s)
+# s = {22, 25, 14, 21, 5}
+# print(s)
 
 
 # Recap --- List,Tuple,set
@@ -24,9 +24,26 @@ print(s)
 
 
 
-# New video - Dictionary -   key-value pairs. {}used in dictionary.  Key should be immutable and unique
-# We can use strings,numbers as keys.
 
-data = {1:'Navin'}
+# //////New video - Dictionary -   key-value pairs. {}used in dictionary.  Key should be 
+# immutable and unique. We can use strings,numbers as keys.
+# when fetching value from Dictionary - []used to mention key
 
+# data = {1: 'Navin', 2: 'Kiran', 4: 'Harsh'}
+# print(data[2])
+# print(data.get(1,'Not found'))
+# print(data.get(3,'Not found'))
 
+# Create a dictionary with the help of list
+keys = ['Navin','Kiran','Harsh']
+values = ['Python','Java','JS']
+
+data1 = dict(zip(keys,values))
+data1['monika'] = 'cs'
+print(data1)
+print(data1['Kiran'])
+
+prog = {'JS':'Atom','CS':'VS','Python':['PyCHarm','Sublime'],'java':{'JSE':'NetBeans','JEE':'Eclipse'}}
+print(prog['JS'])
+print(prog['Python'][1])
+print(prog['java']['JEE'])
