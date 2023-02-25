a7bc7c5419fb00ba3cad5fcea2abee1b39e7cd60
Arman-Kabir <armankabir8@gmail.com>
2023-02-25 20:15:13 +0600

    loop exploring

---
+++ Telusko-Series/21.py
@@ -0,0 +1,9 @@
+# 21  For loop
+# For loop normally works when there is sequence  or a string
+# range(10) means start from 0 and ends at 9.
+
+# x = ['navin', 65, 2.5]
+x= 'navin'
+
+for i in range(10):
+    print(i)
