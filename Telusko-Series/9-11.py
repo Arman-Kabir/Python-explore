77bd214ca91faa0f6e2ce8dedc603a628553692f
Arman-Kabir <armankabir8@gmail.com>
2023-02-25 10:18:09 +0600

    9 done

---
+++ Telusko-Series/9-11.py
@@ -0,0 +1,30 @@
+# 9  // More on variables
+# num = 5
+# name = 'Navin'
+# # print(id(num))
+# #  id() will give me  the memory address
+# a = 10
+# b = a
+# print(a,b)
+# print(id(a))
+# print(id(b))
+# k=10
+# a=9
+# k=a 
+# print(id(10))
+# print(id(k))
+# print(id(a))
+# print(id(b))
+# print(id( k))
+
+
+
+# 10 Data Types
+# None - if variable is not assigned a value
+# Numeric List Tuple Set String Range Dictionary 
+#
+#
+#
+#
+#
+#
