529e30c1892a54e7e9ad573e772bb1ea4b3ebde3
Arman-Kabir <armankabir8@gmail.com>
2023-02-25 22:25:52 +0600

    loop exploring1

---
+++ Telusko-Series/21-22.py
@@ -0,0 +1,36 @@
+
+# 21  For loop
+# For loop normally works when there is sequence  or a string
+# range(10) means start from 0 and ends at 9.
+
+# x = ['navin', 65, 2.5]
+# x = 'navin'
+# for i in range(1, 21):
+#     if (i % 5) == 0:
+#         print(i)
+
+
+# 22 // Break Continue Pass
+# Break means just go outside the loop
+# av = 5
+# x = int(input("How many candies u want?"))
+# i = 1
+# while i <= x:
+#     if i > av:
+#         print("out of stock")
+#         break
+#     print("Candy")
+#     i = i+1
+# print("Bye")
+
+
+
+# for i in range(1, 101):
+#     if (i % 3 == 0 and i % 5 == 0):
+#         continue
+#     print(i)
+
+# print("Bye")
+
+
+
--- Telusko-Series/21.py
+++
@@ -1,9 +0,0 @@
-# 21  For loop
-# For loop normally works when there is sequence  or a string
-# range(10) means start from 0 and ends at 9.
-
-# x = ['navin', 65, 2.5]
-x= 'navin'
-
-for i in range(10):
-    print(i)
