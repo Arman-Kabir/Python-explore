f3c584906c1c06942c18872b44e524dd8c86b194
Arman-Kabir <armankabir8@gmail.com>
2023-02-26 09:19:20 +0600

    26 done

--- Telusko-Series/26-31-Arrays.py
+++ Telusko-Series/26-31-Arrays.py
@@ -5,6 +5,39 @@ from array import *
 # In Arrays, we need to have all the values of same type. Arrays are similar to list but
 #  with one difference that u need to have all the values of same types in array
 # Arrays in python don't have fixed size
+# for loop makes sense when u know the range
+# len(vals) gives u the length of the array
 
-vals = array('I', [5, 9, -8, 4, 2])
-print(vals.buffer_info)
+# vals = array('i', [5, 9, 8, 4, 2])
+# vals = array('u', ['a', 'e', 'i'])
+# print(vals.buffer_info())  #output - (2620241511,5) - 1st one address,2nd one size
+# print(vals.typecode)  # variable type
+# vals.reverse()
+
+# creating a new arrau from an existing array
+# newArr = array(vals.typecode, (a*a for a in vals))
+
+# for i in range(len(vals)):
+#     print(vals[i])
+# /or,
+# for e in newArr:
+#     print(e)
+
+# while loop in array
+# i=0
+# while i < len(newArr):
+#     print(newArr[i])
+#     i+=1
+
+
+# 27 //
+arr = array('i', [])
+
+n = int(input("Enter the length of the array"))
+
+for i in range(n):
+
+    x = int(input("Enter the next value"))
+    arr.append(x)
+
+print(arr)
