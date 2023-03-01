2290e9d7ac00432e7ac500d588c1fe73f660e950
Arman-Kabir <armankabir8@gmail.com>
2023-03-01 10:56:02 +0600

    oop intro

---
+++ OOP-Py-Fcc/1.py
@@ -0,0 +1,26 @@
+
+# Python OOP Concepts
+# Attributes and Objects  # Method Kinds  # Encapsulation  # Abstraction  # Polymorphism  # Abstract Classes  # Store management system
+
+# item1 = 'Phone'
+# item1_price = 100
+# item1_quantity = 5
+# item1_price_total = item1_price * item1_quantity
+#
+# print(type(item1))
+# print(type(item1_price))
+# print(type(item1_quantity))
+# print(type(item1_price_total))
+
+# In Python , Each data type is an object that has been instantiated earlier by some class and for the item1 variablethat has been instantiated from
+# a string type of class. And for the price_quantity and price_total that have been instantiated from a class named int means integer.
+
+# We can create a data type of our own, it would allow us to write a code that we can re-use in the future easily if needed.
+# Now, each instance could have attributes to describe related information about it.
+
+
+
+
+
+
+
